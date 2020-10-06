f344968396e50c8b7078c09382acf95733b8fd13
dvaish <dvaish.edu@gmail.com>
2020-10-06 14:39:21 -0700

    add js function

--- prod/script.js
+++ prod/script.js
@@ -1,1 +1,16 @@
-"use strict";
+'use strict';
+
+const blackholesize = 152
+
+function blackHoles() {
+    let feet = Number(document.getElementById('howmanyfeet').value)
+    let inches = Number(document.getElementById('howmanyinches').value)
+    let output = document.getElementById('output')
+    
+    let total = (feet * 12) + (inches)
+    let answer = blackholesize / total
+
+    output.innerHTML = answer.toFixed(2)
+    alert( output.innerHTML );
+    feet, inches = null;
+}
