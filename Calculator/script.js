7fe4cef694596c215f8eee7e445a363660ec1093
dvaish <dvaish.edu@gmail.com>
2020-10-24 00:55:56 -0700

    full css implementation

--- Calculator/script.js
+++ Calculator/script.js
@@ -2,13 +2,16 @@ let a = 0;
 let b = 0;
 let funcState = null;
 
-
 function display(item) {
+    // General function for passing item to display
     let display = document.getElementById('display');
     display.innerHTML = item;
 }
 
-function operation(operator, x, y) {
+display(0) // Set the display to 0 on load
+
+function operation(operator, x, y) { 
+    // Operator logic in each operation case
     switch (operator) {
         case null:
             return x;
@@ -26,13 +29,16 @@ function operation(operator, x, y) {
 }
 
 function onClick(evt) {
-    let operationEvent = evt.target
+    let operationEvent = evt.target // Identifies the button that was pressed
     switch (operationEvent.className) {
         case 'number':
+            // If button was a number, load it to a
             a = (a == 0) ? operationEvent.innerHTML : (a + operationEvent.innerHTML);
             display(a)
             break;
         case 'function':
+            // If button was a function: store the value of a by pushing it to b, set a to 0 to take a new value
+            // If a and b were both numbers, then the function is valid and will perform the operation
             a = Number(a);
             b = operation(funcState, a, b);
             a = '0';
@@ -40,21 +46,24 @@ function onClick(evt) {
             funcState = operationEvent.id;
             break;
         case 'delete':
+            // Slice the string stored in a to delete the last digit
             a = a.slice(0, -1);
             display(a)
             break;
         case 'clear':
+            // Set all values to 0 or null to reset clear calculator
             a = 0;
             b = 0;
             funcState = null;
             display(a);
             break;
         default:
-            display('empty')
+            display(0)
+            break;
     }
 }
 
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('button'); // Select all buttons in calculator
 for (let i = 0; i < buttons.length; i++ ) {
-    buttons[i].addEventListener('click', onClick);
+    buttons[i].addEventListener('click', onClick); // Add event listener to every button to pass their value to calculator
 }
